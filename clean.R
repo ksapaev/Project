5f21c4f9940c0d13b26fa745088db7ebfa0b2c7d
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-19 13:44:03 -0400

    saving clean.R

--- clean.R
+++ clean.R
@@ -9,7 +9,19 @@ df <- read.csv('raw_data.csv')
 ####################################
 
 ## start writing your R code from here
+library(dplyr)
 
+df$STATE_R <- as.character(df$STATE_R)
+
+project <- df %>% filter(STATE_R %in% state.abb & !is.na(Likelihood_Recommend_H) & !is.na(NPS_Type) & !is.na(Gender_H) & 
+                         !is.na(df$LENGTH_OF_STAY_C) & !is.na(df$Age_Range_H) & !is.na(df$GP_Tier))
+project$STATE_R <- as.factor(project$STATE_R)
+
+
+
+summary(project)
+
+project[project$Guest_Country_H=="Switzerland",]
 ## end your R code and logic 
 
 ####################################
