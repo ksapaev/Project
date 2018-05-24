1a752d146822f894dee88c7f3ab27cbbd5bd289d
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-23 23:14:09 -0400

    saving SVM.R

--- SVM.R
+++ SVM.R
@@ -9,6 +9,50 @@ df <- read.csv('data.csv')
 ####################################
 
 ## start writing your R code from here
+library(ggplot2)
+library(arules)
+library(arulesViz)
+library(kernlab)
+
+#Converting as character
+df$NPS <- as.character(df$NPS)
+#Defining Promoters and passives as Not Detractors
+df$NPS[df$NPS != "Detractor"] <- "NotDetractor"
+#Formatting column as factor
+df$NPS <- as.factor(df$NPS)
+
+
+#Creating indexes
+randomIndex <- sample(1:dim(df)[1])
+summary(randomIndex)
+
+#Setting a cut point
+cutPoint2_3 <- floor(2*dim(df)[1]/3)
+cutPoint2_3
+
+#Creating a train dataframe
+trainData <- df[randomIndex[1:cutPoint2_3],]
+str(trainData)
+length(trainData$NPS)
+#Creating a test dataframe
+testData <- df[randomIndex[(cutPoint2_3+1):dim(df)[1]],]
+str(testData)
+length(testData$NPS)
+
+#Running SVM on train data
+svmOut <- ksvm(NPS ~ ., data=trainData, kernel="rbfdot", kpar="automatic", C=5, prob.model=TRUE)
+
+#Predicting SVM for test data
+svmPredict <- predict(svmOut, testData, type="votes")
+
+#Creating a dataframe for prediction
+compTable <- data.frame(testData[,9], svmPredict[1,])
+str(svmPredict)
+#Showing predicted results in table
+table(compTable)
+str(svmPredict)
+
+
 
 ## end your R code and logic 
 
