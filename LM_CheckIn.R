c7aa35bde07e64cd9d84804e66446c7d97392716
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-23 22:41:06 -0400

    saving LM_CheckIn.R

--- LM_CheckIn.R
+++ LM_CheckIn.R
@@ -12,11 +12,43 @@ df <- read.csv('data.csv')
 View(df)
 str(df)
 
+LinModLTR <- lm(formula=LTR ~ CheckIn, data=df)
+summary(LinModLTR)
+
+LinModLTR <- lm(formula=LTR ~ Hotel_Condition, data=df)
+summary(LinModLTR)
+
+LinModLTR <- lm(formula=LTR ~ Customer_Service, data=df)
+summary(LinModLTR)
+
+LinModLTR <- lm(formula=LTR ~ Hotel_Condition + CheckIn, data=df)
+summary(LinModLTR)
+
 LinModLTR <- lm(formula=LTR ~ Customer_Service + Hotel_Condition + CheckIn, data=df)
 summary(LinModLTR)
 
+
+
+
+
+
+LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ CheckIn, data=df)
+summary(LinModSatisfaction)
+
+LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Hotel_Condition, data=df)
+summary(LinModSatisfaction)
+
+LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Customer_Service, data=df)
+summary(LinModSatisfaction)
+
+LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Hotel_Condition + CheckIn, data=df)
+summary(LinModSatisfaction)
+
 LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Customer_Service + Hotel_Condition + CheckIn, data=df)
 summary(LinModSatisfaction)
+
+View(df)
+str(df)
 ## end your R code and logic 
 
 ####################################
@@ -24,5 +56,6 @@ summary(LinModSatisfaction)
 # add your R code to write output file
 ####################################
 
+getwd()
 
-
+setwd("C:/Users/Khushnud/stigmergy/Project")
