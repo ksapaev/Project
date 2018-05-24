c65582acec99bbd148944335d7c93ffb51533466
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-23 22:53:40 -0400

    saving LM_CheckOut.R

--- LM_CheckOut.R
+++ LM_CheckOut.R
@@ -10,9 +10,21 @@ df <- read.csv('data.csv')
 
 ## start writing your R code from here
 
+LinModLTR <- lm(formula=LTR ~ Staff_Cared, data=df)
+summary(LinModLTR)
+
+LinModLTR <- lm(formula=LTR ~ Room_Satisfy, data=df)
+summary(LinModLTR)
 
 LinModLTR <- lm(formula=LTR ~ Customer_Service + Hotel_Condition + Staff_Cared + Room_Satisfy + LengthStay, data=df)
 summary(LinModLTR)
+
+
+LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Staff_Cared, data=df)
+summary(LinModSatisfaction)
+
+LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Room_Satisfy, data=df)
+summary(LinModSatisfaction)
 
 LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Customer_Service + Hotel_Condition + Staff_Cared + Room_Satisfy + LengthStay, data=df)
 summary(LinModSatisfaction)
