7f55960e1fac689a3baae145fd3dcb44b4a3bdcf
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-27 19:28:10 -0400

    saving LM_CheckOut.R

--- LM_CheckOut.R
+++ LM_CheckOut.R
@@ -52,72 +52,29 @@ dev.off()
 
 
 #LTR - StaffCared + RoomSatisfy + CustomerService + LengthOfStay
+#Length of Stay is statically not significant at 5% level
 LinModLTR <- lm(formula=LTR ~ Customer_Service + Staff_Cared + Room_Satisfy + LengthStay, data=df)
 summary(LinModLTR)
 
 
+#LTR - StaffCared + RoomSatisfy + CustomerService
+LinModLTR <- lm(formula=LTR ~ Customer_Service + Staff_Cared + Room_Satisfy, data=df)
+summary(LinModLTR)
 
 #############
 
-#Linear model for Overall Satisfaction
 
-#OverallSatisfaction - StaffCared
-LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Staff_Cared, data=df)
-summary(LinModSatisfaction)
-
-plot <- ggplot(df, aes(x=Staff_Cared, y=Overall_Satisfaction)) +geom_count()+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE, col = "red") 
-plot <- plot + labs(x = "Staff Cared", y = "Overall Satisfaction")
-plot <- plot  + coord_fixed() + theme_minimal() + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
-
-png(filename="Satisfaction_StaffCared.png", width=800, height=600)
-plot
-dev.off()
-
-#OverallSatisfaction - RoomSatisfy
-LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Room_Satisfy, data=df)
-summary(LinModSatisfaction)
-
-plot <- ggplot(df, aes(x=Room_Satisfy, y=Overall_Satisfaction)) +geom_count()+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE, col = "red") 
-plot <- plot + labs(x = "Room Satisfied", y = "Overall Satisfaction")
-plot <- plot  + coord_fixed() + theme_minimal() + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
-
-png(filename="Satisfaction_RoomSatisfied.png", width=800, height=600)
-plot
-dev.off()
-
-#OverallSatisfaction - CustomerService
-LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Customer_Service, data=df)
-summary(LinModSatisfaction)
-
-plot <- ggplot(df, aes(x=Customer_Service, y=Overall_Satisfaction)) +geom_count()+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE, col = "red") 
-plot <- plot + labs(x = "Customer Service", y = "Overall Satisfaction")
-plot <- plot  + coord_fixed() + theme_minimal() + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
-
-png(filename="Satisfaction_CustomerService.png", width=800, height=600)
-plot
-dev.off()
-
-
-#OverallSatisfaction - StaffCared + RoomSatisfy + CustomerService + LengthOfStay
-LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Customer_Service + Staff_Cared + Room_Satisfy + LengthStay, data=df)
-summary(LinModSatisfaction)
-
-
-
-
-#Create dataframe of just the 10th row
+#Create dataframe of sample 10 elements
 test <- df[sample(df$LTR, 10, replace=FALSE),]
 
-#Test/predict the dataframe with 10th element. Result is Not Detractor for both.
-LTR1test <- predict(LinModLTR, test, type="response")
-ifelse(LTR1test<7, "Detractor", "Not Detractor")
+#Test/predict the dataframe with 10 elements
+LTRtest <- predict(LinModLTR, test, type="response")
+ifelse(LTRtest<7, "Detractor", "Not Detractor")
 
-LTR2test <- predict(LinModSatisfaction, test, type="response")
-ifelse(LTR2test<7, "Detractor", "Not Detractor")
 
 test$LTR
-LTR1test
-LTR2test
+LTRtest
+
 
 ## end your R code and logic 
 
