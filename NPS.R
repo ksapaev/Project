47b7bea1724108fae6d84eab203e15a210a90c38
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-27 17:12:19 -0400

    saving NPS.R

--- NPS.R
+++ NPS.R
@@ -10,6 +10,24 @@ df <- read.csv('data.csv')
 
 ## start writing your R code from here
 
+
+
+#Function to generate NPS
+#Defining a vector equal to the 'Likelihood Recommend' column
+LTR <- df$LTR
+
+#Creating a function of NPS that returns a number
+NPS <- function(LTR) {
+  promoters  <- (sum(LTR > 8))/length(LTR)
+  detractors <- (sum(LTR < 7))/length(LTR)
+  nps <- (promoters-detractors)*100
+  return(nps)
+}
+
+#Checking the value
+NPS(LTR)
+
+
 ## end your R code and logic 
 
 ####################################
