454fde13de33d18a80ae3e5ee2969d94db155dfa
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-27 19:04:30 -0400

    saving LM_CheckOut.R

--- LM_CheckOut.R
+++ LM_CheckOut.R
@@ -61,15 +61,44 @@ summary(LinModLTR)
 
 #Linear model for Overall Satisfaction
 
+#OverallSatisfaction - StaffCared
 LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Staff_Cared, data=df)
 summary(LinModSatisfaction)
 
+plot <- ggplot(df, aes(x=Staff_Cared, y=Overall_Satisfaction)) +geom_count()+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE, col = "red") 
+plot <- plot + labs(x = "Staff Cared", y = "Overall Satisfaction")
+plot <- plot  + coord_fixed() + theme_minimal() + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
+
+png(filename="Satisfaction_StaffCared.png", width=800, height=600)
+plot
+dev.off()
+
+#OverallSatisfaction - RoomSatisfy
 LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Room_Satisfy, data=df)
 summary(LinModSatisfaction)
 
+plot <- ggplot(df, aes(x=Room_Satisfy, y=Overall_Satisfaction)) +geom_count()+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE, col = "red") 
+plot <- plot + labs(x = "Room Satisfied", y = "Overall Satisfaction")
+plot <- plot  + coord_fixed() + theme_minimal() + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
+
+png(filename="Satisfaction_RoomSatisfied.png", width=800, height=600)
+plot
+dev.off()
+
+#OverallSatisfaction - CustomerService
 LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Customer_Service, data=df)
 summary(LinModSatisfaction)
 
+plot <- ggplot(df, aes(x=Customer_Service, y=Overall_Satisfaction)) +geom_count()+ stat_smooth(method = "lm", formula = y ~ x, se = FALSE, col = "red") 
+plot <- plot + labs(x = "Customer Service", y = "Overall Satisfaction")
+plot <- plot  + coord_fixed() + theme_minimal() + scale_x_continuous( breaks = 1:10)+ scale_y_continuous( breaks = 1:10)
+
+png(filename="Satisfaction_CustomerService.png", width=800, height=600)
+plot
+dev.off()
+
+
+#OverallSatisfaction - StaffCared + RoomSatisfy + CustomerService + LengthOfStay
 LinModSatisfaction <- lm(formula=Overall_Satisfaction ~ Customer_Service + Staff_Cared + Room_Satisfy + LengthStay, data=df)
 summary(LinModSatisfaction)
 
@@ -77,7 +106,7 @@ summary(LinModSatisfaction)
 
 
 #Create dataframe of just the 10th row
-test <- df[100:109,]
+test <- df[sample(df$LTR, 10, replace=FALSE),]
 
 #Test/predict the dataframe with 10th element. Result is Not Detractor for both.
 LTR1test <- predict(LinModLTR, test, type="response")
