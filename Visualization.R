5f5d321cb88ff212d3bd5abe5367156cacdc34b0
peaponte <31523022+peaponte@users.noreply.github.com>
2018-05-28 13:43:35 -0400

    saving Visualization.R

--- Visualization.R
+++ Visualization.R
@@ -25,7 +25,7 @@ myPlotLTRReg <- myPlotLTRReg + aes(x=LTR) + aes(y= Region)
 myPlotLTRReg <- myPlotLTRReg + geom_col( fill="blue", col="black") + ggtitle("LTR compared to Region" )
 plot(myPlotLTRReg)
 myPlotLTRReg
-png("myPlotLTRReg")
+
 #Creating proper y and x axis with plot for Revenue compared Room Type
 myPlotRevRoomType <- ggplot(data)
 myPlotRevRoomType <- myPlotRevRoomType + aes(x= Revenue) + aes(y= RoomType)
@@ -33,7 +33,7 @@ myPlotRevRoomType <- myPlotRevRoomType + aes(x= Revenue) + aes(y= RoomType)
 #Choosing color and title
 myPlotRevRoomType <- myPlotRevRoomType + geom_col( fill="green", col="black") + ggtitle("Revenue compared to Room Type" )
 myPlotRevRoomType
-png("myPlotRevRoomType")
+
 #Creating proper y and x axis with plot for Revenue compared Length of Stay
 myPlotRevLength <- ggplot(data)
 myPlotRevLength <- myPlotRevLength + aes(x=Revenue) + aes(y= LengthStay)
@@ -42,7 +42,20 @@ myPlotRevLength <- myPlotRevLength + aes(x=Revenue) + aes(y= LengthStay)
 #Choosing color and title for Revenue compared Length of Stay
 myPlotRevLength <- myPlotRevLength + geom_col( fill="pink", col="black") + ggtitle("Average NPS by state" )
 myPlotRevLength
-png("myPlotRevLength")
+
+
+#Plot to png
+png(filename="myPlotRevLength.png", width=800, height=600)
+plot(myPlotRevLength)
+dev.off()
+
+png(filename="myPlotRevRoomType.png", width=800, height=600)
+plot(myPlotRevRoomType)
+dev.off()
+
+png(filename="myPlotLTRReg.png", width=800, height=600)
+plot(myPlotLTRReg)
+dev.off()
 
 ## end your R code and logic 
 
